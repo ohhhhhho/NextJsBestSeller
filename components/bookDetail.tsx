f1d046ddb83e1639436b012b0c01b1135d64ff90
oh <oh@ohui-MacBookAir.local>
2024-11-16 16:56:16 +0900

    error fix

--- components/bookDetail.tsx
+++ components/bookDetail.tsx
@@ -1,6 +1,7 @@
 import { API_DETAIL } from "@/app/constance";
 import styles from '@/app/styles/bookDetail.module.css'
 import { IBookDetail } from "@/app/type";
+import Image from "next/image";
 
 export async function getDetails(name:string): Promise<IBookDetail>{
     const json = await(await fetch(`${API_DETAIL}/${name}`)).json();
@@ -13,7 +14,7 @@ export default async function BookDetail({name}:{name:string}){
             <div className={styles.wrap}>
                 {detail.results.books.map((i, idx) => (
                     <div key={idx} className={styles.item}>
-                        <img src={i.book_image} alt={i.title} />
+                        <Image src={i.book_image} alt={i.title} width={100} height={100} style={{width:'100%',height:'400px'}}/>
                         <h2>{i.title}</h2>
                         <p>{i.author}</p>
                         <a href={i.amazon_product_url} target="_blank">Buy Now</a>
