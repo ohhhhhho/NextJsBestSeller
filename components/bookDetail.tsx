0b9f0e75c9af2f67acf4eebd53e458daf5e27e60
oh <oh@ohui-MacBookAir.local>
2024-11-16 18:11:26 +0900

    Link tag update

--- components/bookDetail.tsx
+++ components/bookDetail.tsx
@@ -2,6 +2,7 @@ import { API_DETAIL } from "@/app/constance";
 import styles from '@/app/styles/bookDetail.module.css'
 import { IBookDetail } from "@/app/type";
 import Image from "next/image";
+import Link from "next/link";
 
 export async function getDetails(name:string): Promise<IBookDetail>{
     const json = await(await fetch(`${API_DETAIL}/${name}`)).json();
@@ -17,7 +18,7 @@ export default async function BookDetail({name}:{name:string}){
                         <Image src={i.book_image} alt={i.title} width={100} height={100} style={{width:'100%',height:'400px'}}/>
                         <h2>{i.title}</h2>
                         <p>{i.author}</p>
-                        <a href={i.amazon_product_url} target="_blank">Buy Now</a>
+                        <Link href={i.amazon_product_url} target="_blank">Buy Now</Link>
                     </div>
                 ))}
             </div>
